10a83fa7670ac87d53b204f668f6ea48439de632
golam-kibriya123 <golamkibriya622@gmail.com>
2023-04-10 08:13:45 -0500

    added nav bar

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,20 +1,27 @@
 import './NavBar.css'
 import React from 'react';
 import careerHub from '../../assets/allImages/cH.png'
+import menuImg from '../../assets/Icons/menu.svg'
+import crossImg from '../../assets/Icons/cross.svg'
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+
     return (
-        <div className=''>
-            <img src={careerHub} alt="" />
-            <div className="nav-link">
-                <Link to='/'>Home</Link>
-                <Link to='/statistics'>Statistics</Link>
-                <Link to='/appliedJobs'>Applied Jobs</Link>
-                <Link to='/blog'>Blog</Link>
+        <div className='font-serif flex  items-center justify-between p-4 di'>
+            <img src={careerHub} alt="" className='w-44 hidden md:block' />
+            <img src={menuImg} alt="" className='md:hidden w-8' />
+            <img src={crossImg} alt="" className='md:hidden w-9' />
+            <div className="nav-link flex md:flex-row  flex-col">
+                <Link to='/' className='mx-3 text-textD2 hover:text-bgD1'>Home</Link>
+                <Link to='/statistics' className='mx-3 text-textD2 hover:text-bgD1'>Statistics</Link>
+                <Link to='/appliedJobs' className='mx-3 text-textD2 hover:text-bgD1'>Applied Jobs</Link>
+                <Link to='/blog' className='mx-3 text-textD2 hover:text-bgD1'>Blog</Link>
+                <Link to='/blog' className='mx-3 text-textD2 hover:text-bgD1'>Blog</Link>
 
             </div>
-            <h1 className='text-bgD1'>this is header</h1>
+            <button className=" bg-gradient-to-l to-bgL1 from-bgD1 py-4 px-5 rounded-lg  text-textL1 text-xl font-extrabold "
+            >Star Applying</button>
         </div>
     );
 };
